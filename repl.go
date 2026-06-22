946b6ef535a374259f6447f8c9a6bfcf8002f08d
Ok <240349211+reconfirmok@users.noreply.github.com>
2026-06-22 19:39:34 +0300

    feat: REPL

--- repl.go
+++ repl.go
@@ -1,8 +1,34 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+
+		if !scanner.Scan() {
+			break
+		}
+
+		words := cleanInput(scanner.Text())
+		if len(words) == 0 {
+			continue
+		}
+
+		word := words[0]
+		fmt.Printf("Your command was: %s\n", word)
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "reading standard input:", err)
+	}
+}
 
 func cleanInput(text string) []string {
 	lowerText := strings.ToLower(text)
