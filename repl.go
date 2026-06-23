a044b1fb3eda8f335d95447eac12401471ca5005
Ok <240349211+reconfirmok@users.noreply.github.com>
2026-06-23 19:09:06 +0300

    feat: help and exit func

--- repl.go
+++ repl.go
@@ -7,7 +7,14 @@ import (
 	"strings"
 )
 
+type cliCommands struct {
+	name        string
+	description string
+	callback    func() error
+}
+
 func startRepl() {
+
 	scanner := bufio.NewScanner(os.Stdin)
 
 	for {
@@ -22,8 +29,16 @@ func startRepl() {
 			continue
 		}
 
-		word := words[0]
-		fmt.Printf("Your command was: %s\n", word)
+		commandName := words[0]
+		command, exists := getCommands()[commandName]
+		if exists {
+			if err := command.callback(); err != nil {
+				fmt.Println(err)
+			}
+			continue
+		} else {
+			fmt.Println("Unknown command")
+		}
 	}
 	if err := scanner.Err(); err != nil {
 		fmt.Fprintln(os.Stderr, "reading standard input:", err)
@@ -34,4 +49,19 @@ func cleanInput(text string) []string {
 	lowerText := strings.ToLower(text)
 	wordList := strings.Fields(lowerText)
 	return wordList
+}
+
+func getCommands() map[string]cliCommands {
+	return map[string]cliCommands{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
 }
